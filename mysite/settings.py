3fff1782598540c08b9daa6c60613db1b94afd22
so3659 <so3659@naver.com>
2024-10-08 15:36:46 +0900

    Add DEFAULT_PAGINATION_CLASS

--- mysite/settings.py
+++ mysite/settings.py
@@ -134,5 +134,6 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
     # 'rest_framework.permissions.IsAdminUser’,
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
